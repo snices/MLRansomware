11376bac90e23235eb7eaa23ff320576c8781621
snices <bradley.silva@hotmail.com>
2020-06-05 07:03:56 -0700

    Initial idea for mitigation, just shutdown.

--- MLR_v1.py
+++ MLR_v1.py
@@ -1,6 +1,12 @@
 #Capstone ML Ransomware
 #Python3
-import subprocess
-test
+import sys
 
+if sys.platform == 'win32':
+    import ctypes
+    user32 = ctypes.WinDLL('user32')
+    user32.ExitWindowsEx(0x00000008, 0x00000000)
+else:
 
+    import os
+    os.system('sudo shutdown now')
